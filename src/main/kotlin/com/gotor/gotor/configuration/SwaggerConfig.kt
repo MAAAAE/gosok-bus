211072f68aac172bca14625a27e2c2dd1986eef2
Marc Lee <chlee1252@gmail.com>
2024-08-24 15:22:02 +0900

    chore: clean up imports

--- src/main/kotlin/com/gotor/gotor/configuration/SwaggerConfig.kt
+++ src/main/kotlin/com/gotor/gotor/configuration/SwaggerConfig.kt
@@ -1,8 +1,6 @@
 package com.gotor.gotor.configuration
 
-import io.swagger.v3.oas.models.ExternalDocumentation
 import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
 import org.springframework.context.annotation.Bean
