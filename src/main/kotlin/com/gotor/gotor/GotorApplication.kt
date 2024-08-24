b5e5e03cc47e378eef5e96b0d8f55bca0c8dd754
Marc Lee <chlee1252@gmail.com>
2024-08-24 14:55:44 +0900

    Initial Commit :tada:

---
+++ src/main/kotlin/com/gotor/gotor/GotorApplication.kt
@@ -0,0 +1,11 @@
+package com.gotor.gotor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GotorApplication
+
+fun main(args: Array<String>) {
+    runApplication<GotorApplication>(*args)
+}
---
+++ src/test/kotlin/com/gotor/gotor/GotorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gotor.gotor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GotorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
