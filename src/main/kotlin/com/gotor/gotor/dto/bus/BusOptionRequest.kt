14560c33beb26cebacdf67f5ba99a2e46d6e653c
Marc Lee <chlee1252@gmail.com>
2024-08-24 17:36:30 +0900

    chore: create stt directory

--- src/main/kotlin/com/gotor/gotor/dto/bus/BusOptionRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.gotor.gotor.dto.bus
-
-data class BusOptionRequest(
-    val departDate: String,
-    val departTime: String,
-    val from: String,
-    val to: String,
-    val timeOption: String,
-)
--- src/main/kotlin/com/gotor/gotor/service/stt/SttApiClientServiceImpl.kt
+++ src/main/kotlin/com/gotor/gotor/service/stt/SttApiClientServiceImpl.kt
@@ -1,4 +1,4 @@
-package com.gotor.gotor.service
+package com.gotor.gotor.service.stt
 
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
