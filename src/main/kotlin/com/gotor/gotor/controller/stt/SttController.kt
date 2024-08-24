143ba353958bcf727ef6cebc070c1c00563c1f2d
Marc Lee <chlee1252@gmail.com>
2024-08-24 18:44:12 +0900

    feat: added bus mock data to stt

--- src/main/kotlin/com/gotor/gotor/controller/stt/SttController.kt
+++ src/main/kotlin/com/gotor/gotor/controller/stt/SttController.kt
@@ -1,9 +1,13 @@
 package com.gotor.gotor.controller.stt
 
+import com.gotor.gotor.dto.bus.BusOptionResponse
+import com.gotor.gotor.dto.openai.OpenAIBusDto
+import com.gotor.gotor.service.bus.BusService
 import com.gotor.gotor.service.stt.SttApiClientService
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -12,14 +16,23 @@ import org.springframework.web.multipart.MultipartFile
 @Tag(name = "Example API", description = "This API provides examples of different operations.")
 @RestController
 class SttController(
-    private val sttApiClientService: SttApiClientService
+    private val sttApiClientService: SttApiClientService,
+    private val busService: BusService
 ) {
     private val logger: Logger = LoggerFactory.getLogger(SttController::class.java)
 
 
     @PostMapping("/api/stt")
-    fun hello(@RequestParam voice: MultipartFile): String {
+    fun hello(@RequestParam voice: MultipartFile): ResponseEntity<List<BusOptionResponse>> {
         logger.info("[speech to text] API 호출됨.")
-        return sttApiClientService.toText(voice)
+        val text = sttApiClientService.toText(voice)
+        val mockRequest = OpenAIBusDto(
+            departDate = "20180101",
+            departTime = "1100",
+            to = "서울경부",
+            from = "부산",
+            departOption = "0"
+        )
+        return ResponseEntity.ok(busService.getAllAvailableBusOption(mockRequest))
     }
 }
